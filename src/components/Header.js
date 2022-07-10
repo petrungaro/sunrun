f4f3fc8c9650cdc97da86bd8d7b2ef491756b079
Courtney Gibson <courtneygiblets@gmail.com>
2022-07-10 10:59:25 -0400

    put in icons for sunrise/sunset option

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 const Header = () => {
     return(
         <div className='watch'>
-            <img src="/vectorwatch.png" alt="vector of a smart watch" class="vectorImage"></img>
+            <img src="/vectorwatch.png" alt="vector of a smart watch" className="vectorImage"></img>
         </div>
     )
 }
