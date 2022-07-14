45a19aea3b2be3758e56576e73cfff1e5ff8f451
Kiana Omoerah <kiana.omoerah@gmail.com>
2022-07-14 10:55:40 -0400

    code clean up

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,6 @@
 const Footer = () => {
     return(
         <footer>
-            {/* <div className="footerCloud">
-                <p>Created by: "Our names here"</p>
-            </div>
-            <div className="footerCloud">
-                <p><a href="https://www.junocollege.com/" target="_blank" rel="noreferrer">Juno College</a>©️2022</p>
-            </div>
-            <div className="footerCloud">
-                <p>API by <a href="https://sunrise-sunset.org" target="_blank" rel="noreferrer">Sunrise Sunset</a></p>
-            </div> */}
             <div className="footerCloud">
                 <p>Created by: "Our names here" at <a href="https://www.junocollege.com/" target="_blank" rel="noreferrer">Juno College</a></p>
                 <p>API by <a href="https://sunrise-sunset.org" target="_blank" rel="noreferrer">Sunrise Sunset</a></p>
